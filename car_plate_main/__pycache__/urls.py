17bde1bf31954b5d59e2ecc0667931417a4c66b9
peichenleegy <61152216+peichenleegy@users.noreply.github.com>
2020-04-16 07:00:55 +0100

    Add files via upload

---
+++ car_plate_main/__pycache__/urls.py
@@ -0,0 +1,33 @@
+"""car_plate URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from users import views as user_views
+from django.conf import settings\
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls')),
+    path('signup.html/', user_views.signup, name='signup'),
+    path('login.html/', user_views.login, name='login'),
+    path('info.html/', user_views.info, name='info'),
+    path('signup.html/POST', user_views.registration, name='registration'),
+    path('login.html/<email>/availability', user_views.availability, name='availability'),
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
